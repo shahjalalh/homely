90a22cd78dda9a43230db23bfc870a343b612431
Shahjalal Hossain <shossainfolk@gmail.com>
2017-04-17 23:49:52 +0600

    comment in facebook, google and twitter

--- homely/settings.py
+++ homely/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.twitter',
+    # 'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.twitter',
 
     # rest_framework
     'rest_framework',
